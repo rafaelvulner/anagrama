13818c2e0997d7928893e9d5d16d64764a8aa38f
Rafael Monteiro <rafaell.elias@hotmail.com>
2025-03-15 07:51:40 -0300

    Deleted class Seller and create method for generate Anagram

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> letters = List.of("L", "Z", "C", "A");
+        
+        generateAnagram("", letters, letters.size());
+    }
+
+    public static void generateAnagram(String prefix, List<String> letters, int size) {
+        if (prefix.length() == size) {
+            System.out.println(prefix);
+            return;
+        }
+
+        for (int i = 0; i < letters.size(); i++) {
+            List<String> rest = new ArrayList<>(letters);
+            String remove = rest.remove(i);
+            generateAnagram(prefix + remove, rest, size);
+        }
     }
 }
